30ffd00cdc5294024f3832a91a70709675d3b8a7
Negarnbkhti42 <norob35@gmail.com>
2022-03-31 14:19:56 +0430

    contact design

--- src/components/ContactList/index.jsx
+++ src/components/ContactList/index.jsx
@@ -1,3 +1,5 @@
+import "./contactList.scss";
+
 function ContactList({ contacts }) {
   return (
     <div>
@@ -12,12 +14,15 @@ export default ContactList;
 
 function Contact({ data }) {
   return (
-    <div>
-      <span>
+    <div className="contact_wrapper">
+      <div className="contact_info">
         <p>name: {data.name}</p>
         <p>email: {data.email}</p>
-      </span>
-      <button>delete</button>
+      </div>
+      <div className="contact_buttons">
+        <button className="btn_edit">edit</button>
+        <button className="btn_delete">delete</button>
+      </div>
     </div>
   );
 }
