f18638dea76c95ac8001af17cf516b65faf0f1bf
Negarnbkhti42 <norob35@gmail.com>
2022-04-21 18:28:32 +0430

    clean link render

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -1,20 +1,40 @@
 import "./sidebar.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillBookmarkStarFill } from "react-icons/bs";
 
 function Sidebar() {
+  const links = [
+    {
+      title: "home",
+      link: "/",
+      icon: AiFillHome,
+    },
+    {
+      title: "favorites",
+      link: "/",
+      icon: BsFillBookmarkStarFill,
+    },
+  ];
+
   return (
     <div className="sidebar_container">
       <ul className="sidebar_links">
-        <li className="sidebar_link">
-          <Link className="sidebar_item" to="/">
-            home
-          </Link>
-        </li>
-        <li className="sidebar_link">
-          <Link className="sidebar_item" to="/">
-            favorites
-          </Link>
-        </li>
+        {links.map((link, idx) => {
+          return (
+            <li key={idx}>
+              <NavLink
+                className={({ isActive }) =>
+                  `sidebar_item ${isActive ? "sidebar_item-active" : ""}`
+                }
+                to={link.link}
+              >
+                <link.icon className={`sidebar_icon icon-${link.title}`} />
+                {link.title}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
