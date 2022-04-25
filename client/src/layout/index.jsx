9c597a2d0379428dd90d052f402127e5ba75bb77
Negarnbkhti42 <norob35@gmail.com>
2022-04-25 14:57:40 +0430

    setting header

--- client/src/layout/index.jsx
+++ client/src/layout/index.jsx
@@ -3,7 +3,10 @@ import "./layout.scss";
 function Layout({ children }) {
   return (
     <>
-      <div className="layout_container">{children}</div>
+      <div className="layout_container">
+        <header className="layout_header">my contacts</header>
+        {children}
+      </div>
     </>
   );
 }
